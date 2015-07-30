de09138cc0c2afe497d804a202b26907460c4934
tsurkanovm <tsurkanovm@gmail.com>
2015-07-30 15:47:59 +0300

    create init migration

--- console/migrations/m130524_201442_init.php
+++ console/migrations/m130524_201442_init.php
@@ -9,26 +9,88 @@ class m130524_201442_init extends Migration
     {
         $tableOptions = null;
         if ($this->db->driverName === 'mysql') {
-            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
             $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
         }
 
-        $this->createTable('{{%user}}', [
-            'id' => Schema::TYPE_PK,
-            'username' => Schema::TYPE_STRING . ' NOT NULL',
-            'auth_key' => Schema::TYPE_STRING . '(32) NOT NULL',
-            'password_hash' => Schema::TYPE_STRING . ' NOT NULL',
-            'password_reset_token' => Schema::TYPE_STRING,
-            'email' => Schema::TYPE_STRING . ' NOT NULL',
-
-            'status' => Schema::TYPE_SMALLINT . ' NOT NULL DEFAULT 10',
-            'created_at' => Schema::TYPE_INTEGER . ' NOT NULL',
-            'updated_at' => Schema::TYPE_INTEGER . ' NOT NULL',
+     // ======== User ====================================================================
+        $this->createTable('{{%User}}', [
+            'idUser' => Schema::TYPE_INTEGER .'(10) unsigned NOT NULL AUTO_INCREMENT',
+            'email' => Schema::TYPE_STRING . '(45) NOT NULL',
+            'name' => Schema::TYPE_STRING . '(100) NOT NULL',
+            'psw' => Schema::TYPE_STRING . '(60) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL',
+            'gender' => ' enum(\'male\',\'female\') DEFAULT NULL',
+            'role_idrole' => Schema::TYPE_SMALLINT . '(3) unsigned NOT NULL DEFAULT 1',
+            'registration_data' => Schema::TYPE_DATETIME . ' NOT NULL DEFAULT CURRENT_TIMESTAMP',
+            'photo' => Schema::TYPE_STRING . '(255) DEFAULT NULL'
         ], $tableOptions);
+
+        $this->addPrimaryKey('idUser', '{{%User}}', 'idUser' );
+        $this->createIndex( 'ix_email', '{{%User}}', 'email' ,true );
+    // ====================================================================================
+
+    // ======== role ======================================================================
+        $this->createTable('{{%role}}', [
+            'idrole' => Schema::TYPE_SMALLINT . '(3) unsigned NOT NULL AUTO_INCREMENT',
+            'title' => Schema::TYPE_STRING . '(20) NOT NULL'
+        ], $tableOptions);
+
+        $this->addPrimaryKey('idrole', '{{%role}}', 'idrole' );
+        $this->createIndex( 'ix_title', '{{%role}}', 'title' ,true );
+        $this->addForeignKey('User_role_fk','role','idRole','User','role_idrole');
+    // ====================================================================================
+
+    // ======== test ======================================================================
+        $this->createTable('{{%test}}', [
+            'idTest' => Schema::TYPE_SMALLINT . '(3) unsigned NOT NULL AUTO_INCREMENT',
+            'title' => Schema::TYPE_STRING . '(20) NOT NULL',
+            'type' => ' enum(\'usual\',\'control\') DEFAULT NULL'
+        ], $tableOptions);
+
+        $this->addPrimaryKey('idTest', '{{%test}}', 'idTest' );
+        $this->createIndex( 'ix_title', '{{%test}}', 'title' ,true );
+    // ====================================================================================
+
+        // ======== result ====================================================================
+        $this->createTable('{{%result}}', [
+            'idResult' => Schema::TYPE_INTEGER .'(10) unsigned NOT NULL AUTO_INCREMENT',
+            'result' => Schema::TYPE_INTEGER . '(10) NOT NULL',
+            'date_participate' => Schema::TYPE_DATE . ' NOT NULL',
+            'idUser' => Schema::TYPE_INTEGER .'(10) unsigned NOT NULL',
+            'idTest' => Schema::TYPE_SMALLINT . '(3) unsigned NOT NULL'
+        ], $tableOptions);
+
+        $this->addPrimaryKey('idResult', '{{%result}}', 'idResult' );
+        $this->addForeignKey('fk_results_Users','User','idUser','result','idUser');
+        $this->addForeignKey('fk_results_tests','test','idTest','result','idTest');
+        // ====================================================================================
+
+
+        // ======= insert the data ============================================================
+        $role_array = ['idrole' => 1, 'title' => 'Basic'];
+        $this->insert( '{{%role}}',$role_array );
+        $role_array = ['idrole' => 2, 'title' => 'Advance'];
+        $this->insert( '{{%role}}',$role_array );
+        $role_array = ['idrole' => 3, 'title' => 'Admin'];
+        $this->insert( '{{%role}}',$role_array );
+
+        $tests_array = ['title' => 'Math expressions', 'type' => 'usual'];
+        $this->insert( '{{%role}}',$tests_array );
+        $tests_array = ['title' => 'Counting', 'type' => 'control'];
+        $this->insert( '{{%role}}',$tests_array );
+        $tests_array = ['title' => 'Test Stuppa', 'type' => 'control'];
+        $this->insert( '{{%role}}',$tests_array );
+        $tests_array = ['title' => 'Memory test', 'type' => 'control'];
+        $this->insert( '{{%role}}',$tests_array );
+
+        $user_array = ['email' => 'test@test.com', 'name' => 'Admin',
+            'psw' => '96e79218965eb72c92a549dd5a330112',
+            'gender' => 'male','role_idrole' => 3];
+        $this->insert( '{{%User}}',$user_array );
+        // ====================================================================================
     }
 
     public function down()
     {
-        $this->dropTable('{{%user}}');
+        $this->dropTable('{{%User}}');
     }
 }
